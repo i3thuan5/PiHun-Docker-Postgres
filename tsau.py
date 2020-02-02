1ae9d261f41b721e99694514198ba6caa5eab630
fafoy <ihcaoe@gmail.com>
2020-02-02 08:22:00 +0800

    Si-a-geh ho.

--- tsau.py
+++ tsau.py
@@ -11,19 +11,19 @@ def main(sootsai):
         'docker', 'ps',
         '--filter', 'ancestor=postgres:11',
         '--format', '{{.Names}}'
-    ], stdout=PIPE, check=True)
+    ], stdout=PIPE, encoding='utf-8', check=True)
     sikan = date.today().isoformat()
     bokphiau = join(sootsai, sikan)
     makedirs(bokphiau, exist_ok=True)
-    for tsua in ps.stdout.readlines():
+    for tsua in ps.stdout.split('\n'):
         tsiling = docker_tsiling(tsua.rstrip(), bokphiau)
         run(tsiling, shell=True, check=True)
 
 
 def docker_tsiling(mia, bokphiau):
-    print('docker exec {} pg_dump -U postgres | gzip > {}.sql.gz'.format(
+    return 'docker exec {} pg_dump -U postgres | gzip > {}.sql.gz'.format(
         mia, join(bokphiau, tongmia(mia))
-    ))
+    )
 
 
 def tongmia(mia):
